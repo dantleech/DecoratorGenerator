759ca898781325b85d84564ad94ebfaeb584ef64
dantleech <dan.t.leech@gmail.com>
2015-05-11 11:17:57 +0200

    Added generate method to factory

--- lib/DecoratorFactory.php
+++ lib/DecoratorFactory.php
@@ -18,25 +18,49 @@ class DecoratorFactory
         $this->debug = $debug;
     }
 
+    /**
+     * Decorate the given class with the given class name
+     *
+     * @param object $object Object to decorate
+     * @param string $targetClassName Name of the class to generate and decorate with
+     *
+     * @return object
+     */
     public function decorate($object, $targetClassName)
     {
         if (class_exists($targetClassName)) {
             return new $targetClassName($object);
         }
 
+        $this->generate(get_class($object), $targetClassName);
+
+        return new $targetClassName($object);
+    }
+
+    /**
+     * Generate a decorator for the given class with the given target class name
+     *
+     * @param string $className
+     * @param string $targetClassName
+     *
+     * @return string Cache file name
+     */
+    public function generate($className, $targetClassName)
+    {
+        if (class_exists($targetClassName)) {
+            return null;
+        }
+
         $cacheFile = $this->getCacheFile($targetClassName);
 
         if (false === $this->debug && file_exists($cacheFile)) {
             require_once($cacheFile);
-            return new $targetClassName($object);
+            return;
         }
 
-        $code = $this->generator->generate(get_class($object), $targetClassName);
+        $code = $this->generator->generate($className, $targetClassName);
         file_put_contents($cacheFile, '<?php ' . $code);
-
         require_once($cacheFile);
-
-        return new $targetClassName($object);
     }
 
     private function getCacheFile($className)
